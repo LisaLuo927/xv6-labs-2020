cd904a8e5f3a69b8aa70ac691183a4bad2452cd0
ljq <ljq@localhost.localdomain>
2026-06-01 10:56:07 +0000

    lab6: handle cow faults and copyout

--- kernel/vm.c
+++ kernel/vm.c
@@ -397,9 +397,18 @@ int
 copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 {
   uint64 n, va0, pa0;
+  pte_t *pte;
 
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
+
+    if((pte = walk(pagetable, va0, 0)) == 0)
+      return -1;
+    if(*pte & PTE_COW){
+      if(cowalloc(pagetable, va0) < 0)
+        return -1;
+    }
+
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
       return -1;
