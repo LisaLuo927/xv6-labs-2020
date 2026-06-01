d1510ac96ff1b8557d89b9b1b01b68072b2d097b
ljq <ljq@localhost.localdomain>
2026-06-01 05:01:06 +0000

    lab5: pass lazy allocation tests

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -48,7 +48,12 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = p->sz;
-  p->sz += n;
+  if(n < 0){
+    if(growproc(n) < 0)
+      return -1;
+  } else {
+    p->sz += n;
+  }
   return addr;
 }
 
